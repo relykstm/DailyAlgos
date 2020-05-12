5cab60669b28d5be4f7938bd98853fdad3230a25
relykstm <relykstm@gmail.com>
2020-05-12 09:45:11 -0700

    TuesdayMay12

---
+++ ToLowerCase.js
@@ -0,0 +1,9 @@
+var toLowerCase = function(str) {
+    var lower = "";
+    for(i=0; i<str.length; i++){
+        
+        lower = lower + str[i].toLowerCase();
+    }
+    return lower;
+    
+}; 
