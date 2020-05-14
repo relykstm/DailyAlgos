68e2a688fb2bda2087a1bf0b4f7d075001d3456a
relykstm <relykstm@gmail.com>
2020-05-14 10:08:04 -0700

    thursalgo

---
+++ uniquemorsecode.js
@@ -0,0 +1,24 @@
+var uniqueMorseRepresentations = function(words) {
+    var table = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    var arr = [];
+    for(var i=0; i<words.length; i++){
+        var transformation = "";
+        var word = words[i]
+        for(var x=0; x<word.length; x++){
+           var num = word[x].charCodeAt(0) - 97;
+            transformation = transformation + table[num];
+            
+        }
+        arr.push(transformation);
+    }
+    function onlyUnique(value, index, self) { 
+        return self.indexOf(value) === index;
+    }
+    
+    var unique = arr.filter( onlyUnique );
+    console.log(unique)
+    
+    return unique.length;
+    
+
+};
